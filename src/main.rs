ae2a50c862d5d5828c806d11add065e5ceaacd53
Staninna <26458805+Staninna@users.noreply.github.com>
2023-12-18 16:58:54 +0100

    Add "I love you mom!" message to VGA buffer

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,18 @@
 
 mod panic;
 
+static ILY_MOM: &[u8] = b"I love you mom!";
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in ILY_MOM.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
