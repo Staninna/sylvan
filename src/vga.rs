030fd7f9d6e30e4c022fe736d6910f776e60978a
Staninna <26458805+Staninna@users.noreply.github.com>
2023-12-18 21:10:02 +0100

    Update VGA color code to use white instead of yellow

--- src/vga.rs
+++ src/vga.rs
@@ -122,7 +122,7 @@ impl fmt::Write for Writer {
 lazy_static::lazy_static!(
     pub static ref WRITER: spin::Mutex<Writer> = spin::Mutex::new(Writer {
         column_position: 0,
-        color_code: ColorCode::new(Color::Yellow, Color::Black),
+        color_code: ColorCode::new(Color::White, Color::Black),
         buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
     });
 );
