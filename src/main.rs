4e54f458cb0ec70140a29c913d8e6901cfa3994d
Staninna <26458805+Staninna@users.noreply.github.com>
2023-12-18 21:09:54 +0100

    Refactor main.rs to use vga module

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,11 @@
 #![no_main]
 
 mod panic;
-
-static ILY_MOM: &[u8] = b"I love you mom!";
+mod vga;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in ILY_MOM.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    println!("I love you, Mom{}", "!");
 
     loop {}
 }
