b7e9e76c68e72abde2d1ed67596f925a64f2ccff
Staninna <26458805+Staninna@users.noreply.github.com>
2023-12-18 21:08:48 +0100

    Added print macros

--- src/vga.rs
+++ src/vga.rs
@@ -126,3 +126,22 @@ lazy_static::lazy_static!(
         buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
     });
 );
+
+#[doc(hidden)]
+pub fn _print(args: fmt::Arguments) {
+    use core::fmt::Write;
+    WRITER.lock().write_fmt(args).unwrap();
+}
+
+mod macros {
+    #[macro_export]
+    macro_rules! print {
+        ($($arg:tt)*) => ($crate::vga::_print(format_args!($($arg)*)));
+    }
+
+    #[macro_export]
+    macro_rules! println {
+        () => ($crate::print!("\n"));
+        ($($arg:tt)*) => ($crate::print!("{}\n", format_args!($($arg)*)));
+    }
+}
