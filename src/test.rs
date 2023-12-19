d2eba380d852e754ea64a16f39a1633e627c5bf2
Staninna <26458805+Staninna@users.noreply.github.com>
2023-12-19 23:34:57 +0100

    Refactor test runner and add color output

--- src/test.rs
+++ src/test.rs
@@ -1,12 +1,27 @@
-#[cfg(test)]
+#![cfg(test)]
+
 use crate::{
     exit_qemu::{exit_qemu, QemuExitCode},
-    {print, println},
+    {serial_print, serial_println},
 };
 
-#[cfg(test)]
+struct Green(&'static str);
+struct Red(&'static str);
+
+impl core::fmt::Display for Green {
+    fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
+        write!(f, "\x1b[32m{}\x1b[0m", self.0)
+    }
+}
+
+impl core::fmt::Display for Red {
+    fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
+        write!(f, "\x1b[31m{}\x1b[0m", self.0)
+    }
+}
+
 pub fn test_runner(tests: &[&dyn Fn()]) {
-    println!("Running {} tests", tests.len());
+    serial_println!("Running {} tests", tests.len());
     for test in tests {
         test();
     }
@@ -16,7 +31,7 @@ pub fn test_runner(tests: &[&dyn Fn()]) {
 
 #[test_case]
 fn trivial_assertion() {
-    print!("trivial assertion... ");
+    serial_print!("trivial assertion... ");
     assert_eq!(1, 1);
-    println!("[ok]");
+    serial_println!("{}", Green("[ok]"));
 }
