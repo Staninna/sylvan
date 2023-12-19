17fc166f0f3777d2f1b0c7aaf6716db46ccf10ce
Staninna <26458805+Staninna@users.noreply.github.com>
2023-12-19 23:06:10 +0100

    Added basdic code to set up testing

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
 #![no_std]
 #![no_main]
+// Needed for testing
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test::test_runner)]
+#![reexport_test_harness_main = "test"]
 
 mod panic;
+mod test;
 mod vga;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("I love you, Mom{}", "!");
+
+    #[cfg(test)]
+    test();
 
     loop {}
 }
