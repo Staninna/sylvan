314c4d40646a5d9eb6f23f80bebe40b64797a902
Staninna <26458805+Staninna@users.noreply.github.com>
2023-12-20 00:07:29 +0100

    moved 'magic number' into const

--- src/serial.rs
+++ src/serial.rs
@@ -2,9 +2,10 @@ use lazy_static::lazy_static;
 use spin::Mutex;
 use uart_16550::SerialPort;
 
+const COM1: u16 = 0x3F8;
 lazy_static! {
     pub static ref SERIAL1: Mutex<SerialPort> = {
-        let mut serial_port = unsafe { SerialPort::new(0x3F8) };
+        let mut serial_port = unsafe { SerialPort::new(COM1) };
         serial_port.init();
         Mutex::new(serial_port)
     };
