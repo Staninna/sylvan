0af0f414280f18b83f72c2d6101292974f43a581
Staninna <26458805+Staninna@users.noreply.github.com>
2023-12-20 12:09:48 +0100

    Improved test logging

--- src/test.rs
+++ src/test.rs
@@ -29,15 +29,23 @@ where
     T: Fn(),
 {
     fn run(&self) {
-        serial_print!("{}...\t", core::any::type_name::<T>());
+        serial_print!("{:60}", core::any::type_name::<T>());
         self();
         serial_println!("{}", Ok);
     }
 }
 
 pub fn test_runner(tests: &[&dyn Testable]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
+    let count = tests.len();
+    if count == 0 {
+        serial_println!("No tests to run!");
+        exit_qemu(QemuExitCode::Success);
+    }
+
+    serial_println!("Running {} tests", count);
+
+    for (idx, test) in tests.iter().enumerate() {
+        serial_print!("{:>3}/{:<3}", idx + 1, count);
         test.run();
     }
 
