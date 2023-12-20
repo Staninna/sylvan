2274e68fcfd16a7540de29d41976b644bb6cefbe
Staninna <26458805+Staninna@users.noreply.github.com>
2023-12-20 22:23:52 +0100

    Added init code for os and useless loop

--- src/os/main.rs
+++ src/os/main.rs
@@ -5,17 +5,27 @@
 #![test_runner(sylvan_shared::test::test_runner)]
 #![reexport_test_harness_main = "test"]
 
-use sylvan_shared::println;
+use sylvan_shared::{os::init::init, print, println};
 
 mod panic;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    init();
+
+    // Test breakpoint exception
+    x86_64::instructions::interrupts::int3();
+
     println!("I love you, Mom{}", "!");
 
     #[cfg(test)]
     test();
 
-    #[allow(clippy::empty_loop)]
-    loop {}
+    print!("\nEntering useless loop");
+    loop {
+        print!(".");
+        for _ in 0..=1_000_000 {
+            x86_64::instructions::nop();
+        }
+    }
 }
