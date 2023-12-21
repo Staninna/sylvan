469273336eae1ac9b512e1dceedec62f9e86bb3f
Staninna <26458805+Staninna@users.noreply.github.com>
2023-12-21 22:22:25 +0100

    Update panic handler to print FailPrint message

--- src/shared/test/panic.rs
+++ src/shared/test/panic.rs
@@ -1,11 +1,12 @@
 use crate::{
     os::exit_qemu::{exit_qemu, QemuExitCode},
     serial_println,
+    test::FailPrint,
 };
 use core::panic::PanicInfo;
 
 pub fn panic_handler(info: &PanicInfo) -> ! {
-    serial_println!("[failed]\n");
+    serial_println!("{}", FailPrint);
     serial_println!("Error: {}\n", info);
     exit_qemu(QemuExitCode::Failed);
 
