9f8a93c8e3d3cfc30f61029f8ec7acbf52ff8598
Staninna <26458805+Staninna@users.noreply.github.com>
2023-12-22 20:16:31 +0100

    Removed keyboeard handler

--- src/shared/os/interrupts/handlers.rs
+++ src/shared/os/interrupts/handlers.rs
@@ -1,11 +1,8 @@
 use crate::{
     os::interrupts::hardware::{InterruptIndex, PICS},
-    print, println,
+    println,
 };
-use lazy_static::lazy_static;
-use pc_keyboard::{layouts::Us104Key, DecodedKey, HandleControl, Keyboard, ScancodeSet1};
-use spin::Mutex;
-use x86_64::{instructions::port::Port, structures::idt::InterruptStackFrame};
+use x86_64::structures::idt::InterruptStackFrame;
 
 pub extern "x86-interrupt" fn breakpoint_exception_handler(stack_frame: InterruptStackFrame) {
     println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
@@ -24,33 +21,5 @@ pub extern "x86-interrupt" fn timer_interrupt_handler(_stack_frame: InterruptSta
     unsafe {
         PICS.lock()
             .notify_end_of_interrupt(InterruptIndex::Timer.u8());
-    }
-}
-
-lazy_static! {
-    static ref KEYBOARD: Mutex<Keyboard<Us104Key, ScancodeSet1>> = Mutex::new(Keyboard::new(
-        ScancodeSet1::new(),
-        Us104Key,
-        HandleControl::Ignore
-    ));
-}
-
-pub extern "x86-interrupt" fn keyboard_interrupt_handler(_stack_frame: InterruptStackFrame) {
-    let mut keyboard = KEYBOARD.lock();
-    let mut port = Port::new(0x60);
-
-    let scancode: u8 = unsafe { port.read() };
-    if let Ok(Some(key_event)) = keyboard.add_byte(scancode) {
-        if let Some(key) = keyboard.process_keyevent(key_event) {
-            match key {
-                DecodedKey::Unicode(character) => print!("{}", character),
-                DecodedKey::RawKey(key) => print!("{:?}", key),
-            }
-        }
-    }
-
-    unsafe {
-        PICS.lock()
-            .notify_end_of_interrupt(InterruptIndex::Keyboard.u8());
     }
 }
