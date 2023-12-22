eb3653f54904749bbf164063925204c20ef17c60
Staninna <26458805+Staninna@users.noreply.github.com>
2023-12-22 16:32:32 +0100

    Enable interupts on CPU and added copmments

--- src/shared/os/init.rs
+++ src/shared/os/init.rs
@@ -1,6 +1,11 @@
 pub fn init() {
+    // Initialize the Global Descriptor Table (GDT) and the Task State Segment (TSS)
     super::gdt::init();
+
+    // Initialize the Interrupt Descriptor Table (IDT)
     super::interrupts::init_idt();
 
+    // Initialize the PICs (Programmable Interrupt Controllers)
     unsafe { super::interrupts::PICS.lock().initialize() };
+    x86_64::instructions::interrupts::enable();
 }
