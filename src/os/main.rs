e5041a6be672fdd820eba8f03c884ba40c3975aa
Staninna <26458805+Staninna@users.noreply.github.com>
2023-12-22 14:22:13 +0100

    Print rick roll in main loop

--- src/os/main.rs
+++ src/os/main.rs
@@ -5,25 +5,35 @@
 #![test_runner(sylvan_shared::test::test_runner)]
 #![reexport_test_harness_main = "test"]
 
+use arrayvec::ArrayString;
 use sylvan_shared::{os::init::init, print, println};
 
 mod panic;
+
+const BUFFER_CAP: usize = 25 * 80;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     init();
 
-    println!("I love you, Mom{}", "!");
-    println!("I also love you, Dad{}", "!");
+    let rick = include_str!("../../ascii_art.txt");
 
-    #[cfg(test)]
-    test();
-
-    print!("\nEntering useless loop");
     loop {
-        print!(".");
-        for _ in 0..=1_000_000 {
-            x86_64::instructions::nop();
+        // Print each line of the ascii art
+        let mut buffer: ArrayString<BUFFER_CAP> = ArrayString::new();
+        for line in rick.lines() {
+            // If the line has - in it, wait 1000/30 ms
+            if line.contains('-') {
+                buffer.push_str(line);
+                print!("{}", buffer);
+                buffer.clear();
+                for _ in 0..100_000_000 {
+                    x86_64::instructions::nop();
+                }
+                continue;
+            } else {
+                buffer.push_str(line);
+            }
         }
     }
 }
