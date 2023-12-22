50190aa52f8804837c78cad2f1e917cf4b2fc47d
Staninna <26458805+Staninna@users.noreply.github.com>
2023-12-22 19:29:01 +0100

    Made keyboard more usable

--- src/shared/os/interrupts/handlers.rs
+++ src/shared/os/interrupts/handlers.rs
@@ -2,7 +2,10 @@ use crate::{
     os::interrupts::hardware::{InterruptIndex, PICS},
     print, println,
 };
-use x86_64::structures::idt::InterruptStackFrame;
+use lazy_static::lazy_static;
+use pc_keyboard::{layouts::Us104Key, DecodedKey, HandleControl, Keyboard, ScancodeSet1};
+use spin::Mutex;
+use x86_64::{instructions::port::Port, structures::idt::InterruptStackFrame};
 
 pub extern "x86-interrupt" fn breakpoint_exception_handler(stack_frame: InterruptStackFrame) {
     println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
@@ -24,45 +27,26 @@ pub extern "x86-interrupt" fn timer_interrupt_handler(_stack_frame: InterruptSta
     }
 }
 
+lazy_static! {
+    static ref KEYBOARD: Mutex<Keyboard<Us104Key, ScancodeSet1>> = Mutex::new(Keyboard::new(
+        ScancodeSet1::new(),
+        Us104Key,
+        HandleControl::Ignore
+    ));
+}
+
 pub extern "x86-interrupt" fn keyboard_interrupt_handler(_stack_frame: InterruptStackFrame) {
-    use x86_64::instructions::port::Port;
-
+    let mut keyboard = KEYBOARD.lock();
     let mut port = Port::new(0x60);
-    let scancode: u8 = unsafe { port.read() };
 
-    let key = match scancode {
-        0x1e => Some('a'),  // A
-        0x30 => Some('b'),  // B
-        0x2e => Some('c'),  // C
-        0x20 => Some('d'),  // D
-        0x12 => Some('e'),  // E
-        0x21 => Some('f'),  // F
-        0x22 => Some('g'),  // G
-        0x23 => Some('h'),  // H
-        0x17 => Some('i'),  // I
-        0x24 => Some('j'),  // J
-        0x25 => Some('k'),  // K
-        0x26 => Some('l'),  // L
-        0x32 => Some('m'),  // M
-        0x31 => Some('n'),  // N
-        0x18 => Some('o'),  // O
-        0x19 => Some('p'),  // P
-        0x10 => Some('q'),  // Q
-        0x13 => Some('r'),  // R
-        0x1f => Some('s'),  // S
-        0x14 => Some('t'),  // T
-        0x16 => Some('u'),  // U
-        0x2f => Some('v'),  // V
-        0x11 => Some('w'),  // W
-        0x2d => Some('x'),  // X
-        0x15 => Some('y'),  // Y
-        0x2c => Some('z'),  // Z
-        0x39 => Some(' '),  // Space
-        0x1c => Some('\n'), // Enter
-        _ => None,
-    };
-    if let Some(key) = key {
-        print!("{}", key);
+    let scancode: u8 = unsafe { port.read() };
+    if let Ok(Some(key_event)) = keyboard.add_byte(scancode) {
+        if let Some(key) = keyboard.process_keyevent(key_event) {
+            match key {
+                DecodedKey::Unicode(character) => print!("{}", character),
+                DecodedKey::RawKey(key) => print!("{:?}", key),
+            }
+        }
     }
 
     unsafe {
